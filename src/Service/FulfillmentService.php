d03f618db0db02ebff841f3e92594e17f27ce849
chewlim <chew@yogee.com.au>
2021-11-23 12:24:29 +1100

    Fixed typo on fulfilment service

--- src/Service/FulfillmentService.php
+++ src/Service/FulfillmentService.php
@@ -8,21 +8,21 @@ class FulfillmentService extends AbstractService
 {
     public function all($orderId, array $params = [])
     {
-        $endpoint = 'orders/'.$orderId.'/fulfillments.json';
+        $endpoint = 'orders/' . $orderId . '/fulfillments.json';
         $response = $this->request($endpoint, 'GET', $params);
         return $this->createCollection(Fulfillment::class, $response['fulfillments']);
     }
 
     public function count($orderId, array $params = [])
     {
-        $endpoint = 'orders/'.$orderId.'/fulfillments/count.json';
+        $endpoint = 'orders/' . $orderId . '/fulfillments/count.json';
         $response = $this->request($endpoint, 'GET', $params);
         return $response['count'];
     }
 
     public function get($orderId, $fulfillmentId, array $params = [])
     {
-        $endpoint = 'orders/'.$orderId.'/fulfillments/'.$fulfillmentId.'.json';
+        $endpoint = 'orders/' . $orderId . '/fulfillments/' . $fulfillmentId . '.json';
         $response = $this->request($endpoint, 'GET', $params);
         return $this->createObject(Fulfillment::class, $response['fulfillments']);
     }
@@ -30,11 +30,13 @@ class FulfillmentService extends AbstractService
     public function create($orderId, Fulfillment &$fulfillment)
     {
         $data = $fulfillment->exportData();
-        $endpoint = 'orders/'.$orderId.'/fulfillments.json';
+        $endpoint = 'orders/' . $orderId . '/fulfillments.json';
         $response = $this->request(
-            $endpoint, 'POST', array(
-            'fulfillment' => $data
-            )
+            $endpoint,
+            'POST',
+            [
+                'fulfillment' => $data
+            ]
         );
         $fulfillment->setData($response['fulfillment']);
     }
@@ -42,32 +44,34 @@ class FulfillmentService extends AbstractService
     public function update($orderId, Fulfillment &$fulfillment)
     {
         $data = $fulfillment->exportData();
-        $endpoint = 'orders/'.$orderId.'/fulfillments/'.$fulfillment->gtId().'.json';
+        $endpoint = 'orders/' . $orderId . '/fulfillments/' . $fulfillment->getId() . '.json';
         $response = $this->request(
-            $endpoint, 'POST', array(
-            'fulfillment' => $data
-            )
+            $endpoint,
+            'POST',
+            [
+                'fulfillment' => $data
+            ]
         );
         $fulfillment->setData($response['fulfillment']);
     }
 
     public function complete($orderId, Fulfillment &$fulfillment)
     {
-        $endpoint = 'orders/'.$orderId.'/fulfillments/'.$fulfillment->getId().'/complete.json';
+        $endpoint = 'orders/' . $orderId . '/fulfillments/' . $fulfillment->getId() . '/complete.json';
         $response = $this->request($endpoint, 'POST');
         $fulfillment->setData($response['fulfillment']);
     }
 
     public function cancel($orderId, Fulfillment &$fulfillment)
     {
-        $endpoint = 'orders/'.$orderId.'/fulfillments/'.$fulfillment->getId().'/cancel.json';
+        $endpoint = 'orders/' . $orderId . '/fulfillments/' . $fulfillment->getId() . '/cancel.json';
         $response = $this->request($endpoint, 'POST');
         $fulfillment->setData($response['fulfillment']);
     }
 
     public function open($orderId, Fulfillment &$fulfillment)
     {
-        $endpoint = 'orders/'.$orderId.'/fulfillments/'.$fulfillment->getId().'/open.json';
+        $endpoint = 'orders/' . $orderId . '/fulfillments/' . $fulfillment->getId() . '/open.json';
         $response = $this->request($endpoint, 'POST');
         $fulfillment->setData($response->fulfillment);
     }
