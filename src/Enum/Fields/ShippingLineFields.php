17770559ac897497ad485545f25eb0866831d7d0
chewlim <chew@yogee.com.au>
2021-12-05 22:12:32 +1100

    added discount_allocations to ShippingLineFields

--- src/Enum/Fields/ShippingLineFields.php
+++ src/Enum/Fields/ShippingLineFields.php
@@ -4,24 +4,31 @@ namespace Shopify\Enum\Fields;
 
 class ShippingLineFields extends AbstractObjectEnum
 {
+    const ID = 'id';
     const CODE = 'code';
     const PRICE = 'price';
     const SOURCE = 'source';
     const TITLE = 'title';
     const TAX_LINES = 'tax_lines';
     const CARRIER_IDENTIFIER = 'carrier_identifier';
+    const DELIVERY_CATEGORY = 'delivery_category';
     const REQUESTED_FULFILLMENT_SERVICE_ID = 'requested_fulfillment_service_id';
+    const DISCOUNT_ALLOCATIONS = 'discount_allocations';
 
     public function getFieldTypes()
     {
-        return array(
+        return [
+            'id' => 'integer',
             'code' => 'string',
             'price' => 'string',
             'source' => 'string',
             'title' => 'string',
             'tax_lines' => 'TaxLine[]',
             'carrier_identifer' => 'string',
-            'requested_fulfillment_service_id' => 'string'
-        );
+            'delivery_category' => 'string',
+            'discounted_price' => 'string',
+            'requested_fulfillment_service_id' => 'string',
+            'discount_allocations' => 'DiscountAllocation[]',
+        ];
     }
 }
